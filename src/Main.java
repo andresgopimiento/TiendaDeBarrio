dba591ce33daa6a8af9e68e25b04217948205a3c
Andrés Gómez Pimiento <mrandrespimiento@gmail.com>
2023-10-17 16:34:56 -0500

    Se actualiza la información

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import products.ProductsArray;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        InventoryApp app = new InventoryApp();
+        app.runMenu();
+        */
+
+        ProductsArray productsInstance = new ProductsArray();
+        productsInstance.mostrarProductos();
+
+    }
+}
