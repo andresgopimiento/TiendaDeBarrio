5374af7f0d6461182d89be7486b78f2be90d190c
Andrés Gómez Pimiento <mrandrespimiento@gmail.com>
2023-10-18 14:31:36 -0500

    Se agrega un paquete llamado 'model' y se agrega una clase 'Producto'

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import model.Product;
 import products.ProductsArray;
 
 public class Main {
@@ -5,10 +6,26 @@ public class Main {
         /*
         InventoryApp app = new InventoryApp();
         app.runMenu();
-        */
+
 
         ProductsArray productsInstance = new ProductsArray();
         productsInstance.mostrarProductos();
+
+         */
+
+        Product product = new Product("Laptop", 999.99, 5);
+
+        // Verificar si el producto está sin inventario
+        System.out.println("¿El producto está sin inventario? " + product.isOutOfStock());
+
+        // Verificar si el precio del producto es mayor a un valor
+        System.out.println("¿El precio del producto es mayor a 500? " + product.isPriceGreaterThan(500.0));
+
+        // Verificar si el precio del producto es menor o igual a un valor
+        System.out.println("¿El precio del producto es menor o igual a 1000? " + product.isPriceLessThanOrEqualTo(1000.0));
+
+        // Verificar si el nombre del producto contiene una palabra
+        System.out.println("¿El nombre del producto contiene 'top'? " + product.containsKeyword("top"));
 
     }
 }
